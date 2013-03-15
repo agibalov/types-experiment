43a8e6a2bf1828b54d82249ac55b587333c71842
loki2302 <2302johnsmith@gmail.com>
2013-03-15 16:06:24 -0700

    Progress with DOM: assignment and variable reference

--- src/main/java/com/loki2302/dom/DOMAssignmentExpression.java
+++ src/main/java/com/loki2302/dom/DOMAssignmentExpression.java
@@ -1,6 +1,25 @@
 package com.loki2302.dom;
 
 public class DOMAssignmentExpression implements DOMExpression {
+	private final DOMExpression leftExpression; 
+	private final DOMExpression rightExpression;
+	
+	public DOMAssignmentExpression(
+			DOMExpression leftExpression, 
+			DOMExpression rightExpression) {
+		
+		this.leftExpression = leftExpression;
+		this.rightExpression = rightExpression;
+	}
+	
+	public DOMExpression getLeftExpression() {
+		return leftExpression;
+	}
+	
+	public DOMExpression getRightExpression() {
+		return rightExpression;
+	}
+	
 	@Override
 	public <T> T accept(DOMExpressionVisitor<T> visitor) {
 		return visitor.visitDOMAssignmentExpression(this);
--- src/main/java/com/loki2302/dom/DOMVariableReferenceExpression.java
+++ src/main/java/com/loki2302/dom/DOMVariableReferenceExpression.java
@@ -1,6 +1,16 @@
 package com.loki2302.dom;
 
 public class DOMVariableReferenceExpression implements DOMExpression {
+	private final String variableName;
+	
+	public DOMVariableReferenceExpression(String variableName) {
+		this.variableName = variableName;
+	}
+	
+	public String getVariableName() {
+		return variableName;
+	}
+	
 	@Override
 	public <T> T accept(DOMExpressionVisitor<T> visitor) {
 		return visitor.visitDOMVariableReferenceExpression(this);
